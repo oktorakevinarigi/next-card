ba4b6c13e4ec1778de2379f1334776e6c0d6d7ab
Oktora Kevin Arigi <oktora.kevinarigi@ai.astra.co.id>
2021-05-06 12:13:54 +0700

    change url image

--- pages/[id].js
+++ pages/[id].js
@@ -27,7 +27,7 @@ export default function Home({data}) {
         <meta property="og:site_name" content="youtube" />
         <meta property="og:url" content={data.card.videoURL} />
         <meta property="og:title" content="Astra Greeting Card" />
-        <meta property="og:image" content={data.card.thumbnails} />
+        <meta property="og:image" content="images/logo1.png" />
         <meta property="og:type" content="video.other" />
         <meta property="og:video:url" content={data.card.videoURL} />
         <meta property="og:video:secure_url" content={data.card.videoURL} />
@@ -44,7 +44,7 @@ export default function Home({data}) {
         <meta name="twitter:site" content="@youtube" />
         <meta name="twitter:title" content="Astra Greeting Card" />
         <meta name="twitter:description" content={data.card.title} />
-        <meta name="twitter:image" content={data.card.thumbnails} />
+        <meta name="twitter:image" content="images/logo1.png" />
         <meta name="twitter:player" content={data.card.videoURL} />
         <meta name="twitter:player:width" content="1280" />
         <meta name="twitter:player:height" content="720" />
